17ce81ff2dd19139be7a62900cf02d45c21c136c
SiriFazendoBarra <manuel.martinez.perezdearce@gmail.com>
2023-03-31 11:31:57 -0300

    2nd, firebase is working, we are online people :)

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -5,25 +5,35 @@ const DataContext = createContext()
 
 export default function DataProvider({ children }) {
     const API_URL = import.meta.env.VITE_API_URL
-    const [data, setData] = useState([])
+    const [info, setInfo] = useState([])
+    const [portafolio, setPortafolio] = useState([])
 
     const getData = async () => {
         try {
-            const { data } = await axios.get(API_URL)
-            setData(data.portafolio)
-            console.log(data)
+            const { data } = await axios.get(`${API_URL}.json`)
+            setInfo(data)
+
         } catch (error) {
             console.log(error)
         }
-
     }
+
+    const getPortafolio = () => {
+        info.portafolio ? setPortafolio(Object.values(info.portafolio)) : null
+    }
+
+    useEffect(() => {
+        getPortafolio()
+    }, [info])
 
     useEffect(() => {
         getData()
     }, [])
+    console.log(info)
+    console.log(portafolio)
 
     return (
-        <DataContext.Provider value={{ data }}>
+        <DataContext.Provider value={{ portafolio }}>
             {children}
         </DataContext.Provider>
     )
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,15 +3,22 @@ import { useDataContext } from '../context/DataContext'
 
 export default function Home() {
 
-    const { data } = useDataContext()
+    const { portafolio } = useDataContext()
 
     return (
         <div className="container-fluid my-3 ">
             <div className="container d-flex flex-column gap-3">
                 <p className='h2'>Portafolio</p>
-                <section className='portafolioSection'>
-                    <Card data={data} />
+                <section className='portafolioSection gap-3'>
 
+                    {
+                        portafolio.map((element) => {
+
+                            return (
+                                <Card key={element.title} element={element} />
+                            )
+                        })
+                    }
                 </section>
             </div>
         </div>
