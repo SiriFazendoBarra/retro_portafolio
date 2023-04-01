8c41a909807268f2c66e256e8b313eea85312de0
SiriFazendoBarra <manuel.martinez.perezdearce@gmail.com>
2023-04-01 18:24:17 -0300

    4, minor changes to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,18 @@
 export default function Card({ element }) {
+    console.log(element.img)
     return (
         <div className="cards d-flex flex-column justify-content-between bg-primary text-black nes-container with-title">
             <h3 className="title bg-primary">{element.title}</h3>
-            <img className="card-img" src="https://d33wubrfki0l68.cloudfront.net/641dda4e8bdd9115fc92acbe/screenshot_2023-03-24-17-13-53-0000.png" alt="pic" />
+            <img className="card-img" src={element.img} alt="pic" />
             <div className="card-body d-flex flex-column justify-content-between my-2">
-                <p className="card-text m-0">{element.descr}</p>
+                <div>
+                    <p className="card-date mb-2 text-end">{element.date}</p>
+                    <p className="card-text m-0">{element.descr}</p>
+                </div>
+
                 <div className="d-flex justify-content-between">
                     <a href={element.repo} target="_blank"><i className="nes-icon github is-medium"></i></a>
-                    <a type='button' className="text-decoration-none nes-btn is-primary" href={element.deploy} target="_blank">Deploy</a>
+                    <a className="text-decoration-none nes-btn is-primary" href={element.deploy} target="_blank">Deploy</a>
                 </div>
             </div>
         </div>
